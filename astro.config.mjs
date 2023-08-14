1a2015cf0adf98f0f398d93261ea34255d36bd47
andijayawizard <andijayawizard@gmail.com>
2023-08-14 14:35:59 +0700

    1408

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-
 import mdx from "@astrojs/mdx";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind(), react(), sitemap(), mdx()]
+  integrations: [preact(), tailwind(), react(), sitemap(), mdx()],
+  output: "server",
+  adapter: vercel()
 });
